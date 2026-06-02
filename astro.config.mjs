beecf0eb05ab9eb1b2b0f59e51a8e2f4aa477301
OliverL722 <113717885+OliverL722@users.noreply.github.com>
2026-06-02 10:41:09 -0500

    funny image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ololesiakowski.github.io',
-  base: '/engineering-portfolio',
+  site: 'https://oliverlesiakowski.com',
 });
